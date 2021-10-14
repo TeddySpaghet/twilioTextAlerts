1a5304dd1fad53d8b39fee8a4e69f09fe24443d6
theodore pochart <theodore.pochart@gmail.com>
2021-10-14 15:19:34 -0700

    sending and receiving messages functional

--- send_sms.js
+++ send_sms.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
-     to: '+15558675310'
-   })
-  .then(message => console.log(message.sid));
+    body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+    from: "+17742688073",
+    to: "+19143185782",
+  })
+  .then((message) => console.log(message.sid));
